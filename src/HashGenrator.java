d4392b7578592b377c47b6c3e954bcfeb3d15338
Najlaalrajhia98 <najla.alrajhi@tra.gov.om>
2022-12-07 17:54:43 +0400

    Readme

--- src/HashGenrator.java
+++ src/HashGenrator.java
@@ -12,17 +12,13 @@ public class HashGenrator {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		class HashGenrator {
 			
-			public static void main(String[] args) {
 				
 				System.out.println("Select am action:");
 				System.out.println("---------------");
 				System.out.println("1) Generate Hash ");
 				System.out.println("2) Compare Hash ");
 				System.out.println("3) Re-compute Hash ");
-			}
-		} 
+		}
 	}
 
-}
