4399c9672693e30dcf392adcc2db512c6f3b5e2b
Najlaalrajhia98 <119934550+Najlaalrajhia98@users.noreply.github.com>
2022-12-07 22:06:45 +0400

    Update HashGenrator.java

--- src/HashGenrator.java
+++ src/HashGenrator.java
@@ -17,7 +17,7 @@ public class HashGenrator {
 				System.out.println("Select am action:");
 				System.out.println("---------------");
 				System.out.println("1) Generate Hashes ");
-				System.out.println("2) Compare Hashing ");
+				System.out.println("2) Compare Hashinnnng ");
 				System.out.println("3) Re-compute Hash ");
 		}
 	}
