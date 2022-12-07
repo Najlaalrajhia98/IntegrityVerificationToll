4a2d05426a88202b19f0e697c43640ee2cf2b689
Najlaalrajhia98 <najla.alrajhi@tra.gov.om>
2022-12-07 14:03:39 +0400

    cleared the Repo and copy new eclipse java project

--- README.md
+++
@@ -1,2 +0,0 @@
-# IntegrityVerificationToll
-This project is used to generate hashes of files that we can compare those hashes to verify the integrity of files
---
+++ src/HashGenrator.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+/**
+ * this class can be used to generate of files and strings..
+ *
+ */
+public class HashGenrator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		class HashGenrator {
+			
+			public static void main(String[] args) {
+				
+				System.out.println("Select am action:");
+				System.out.println("---------------");
+				System.out.println("1) Generate Hash ");
+				System.out.println("2) Compare Hash ");
+				System.out.println("3) Re-compute Hash ");
+			}
+		} 
+	}
+
+}
